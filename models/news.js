2ce2d81743f177e3608b68009a7d203ddd19ba8c
Shakhriyor11 <shaxrik113099@gmail.com>
2021-06-28 22:57:02 +0500

    to'g'irlandi

--- models/news.js
+++ models/news.js
@@ -8,8 +8,14 @@ const newsSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    categoryID:{
+        type:mongoose.ObjectId,
+        required:true
+    },
     date:{
         type:Date,
         default:Date.now()
     }
 })
+
+module.exports = mongoose.model('news',newsSchema)
