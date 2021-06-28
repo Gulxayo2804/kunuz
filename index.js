fd8b7de1542ac23f2b6e42166d8d0383e172ade5
Sarvarjon <sarvaregamberdiyev758@gmail.com>
2021-06-28 23:17:57 +0500

    index

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Salom')
+const express=require(`express`);
+const app=express();
+const bodyParser=require(`body-parser`)
+const PORT=5000;
+app.use(bodyParser.json());
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server on ${PORT}  running `);
+})
